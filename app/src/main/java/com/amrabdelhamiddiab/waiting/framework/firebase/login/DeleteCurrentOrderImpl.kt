7805b42cef7b3ba4c030037522c4a5ac8e08ddce
engam <engamrdiab52@gmail.com>
2022-08-08 17:52:53 +0200

    implement opn qr code

--- app/src/main/java/com/amrabdelhamiddiab/waiting/framework/firebase/login/DeleteCurrentOrderImpl.kt
+++ app/src/main/java/com/amrabdelhamiddiab/waiting/framework/firebase/login/DeleteCurrentOrderImpl.kt
@@ -1,25 +1,20 @@
 package com.amrabdelhamiddiab.waiting.framework.firebase.login
 
-import android.content.Context
 import android.util.Log
-import android.widget.Toast
 import com.amrabdelhamiddiab.core.data.login.IDeleteCurrentOrder
 import com.amrabdelhamiddiab.waiting.MainActivity
 import com.google.firebase.database.DatabaseReference
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
-class DeleteCurrentOrderImpl @Inject constructor(
-    private val databaseReference: DatabaseReference,
-    @ApplicationContext private val context: Context
-) :
+class DeleteCurrentOrderImpl @Inject constructor(private val databaseReference: DatabaseReference) :
     IDeleteCurrentOrder {
     override suspend fun deleteCurrentOrder(userId: String) {
         try {
             databaseReference.child("orders").child(userId).removeValue().await()
-        } catch (e: Exception) {
-            Toast.makeText(context, e.message, Toast.LENGTH_LONG).show()
+        }catch (e: Exception){
+            Log.d(MainActivity.TAG, e.message.toString())
+
         }
     }
 }
