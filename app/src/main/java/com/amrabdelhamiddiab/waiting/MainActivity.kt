e030df339f4341d80993151e202d481702a8cf9b
EngAmr <engamrdiab52@gmail.com>
2022-08-11 19:22:34 +0200

    add toolbar and navigation drawer

--- app/src/main/java/com/amrabdelhamiddiab/waiting/MainActivity.kt
+++ app/src/main/java/com/amrabdelhamiddiab/waiting/MainActivity.kt
@@ -2,32 +2,76 @@ package com.amrabdelhamiddiab.waiting
 
 import android.content.Context
 import android.os.Bundle
-import android.speech.tts.TextToSpeech
-import android.speech.tts.Voice
-import android.util.Log
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.amrabdelhamiddiab.core.data.IPreferenceHelper
+import com.amrabdelhamiddiab.waiting.databinding.ActivityMainBinding
+import com.amrabdelhamiddiab.waiting.framework.utilis.PreferenceManager
+import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.*
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
     companion object {
         const val TAG = "MainActivity"
-        const val TOPIC = "/topics/myTopic"
     }
+
+    private val navController: NavController by lazy {
+        Navigation.findNavController(this, R.id.nav_host_fragment_content_main)
+    }
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navigationView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toolbar: Toolbar
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var preferenceHelper: IPreferenceHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-       // Firebase.messaging.isAutoInitEnabled = true
-        MyFirebaseMessagingService.sharedPref = getSharedPreferences("sharedPrefToken", Context.MODE_PRIVATE)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        MyFirebaseMessagingService.sharedPref =
+            getSharedPreferences("sharedPrefToken", Context.MODE_PRIVATE)
 
-    //    FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+        preferenceHelper = PreferenceManager(this.applicationContext)
 
-
-
+        drawerLayout = binding.drawerLayout
+        navigationView = binding.navigationView
+        toolbar = binding.appBarMain.toolbar
+        setSupportActionBar(toolbar)
+        navigationView.setupWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.currentBalanceFragment,
+                R.id.homeFragment
+            ), drawerLayout
+        )
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+    }
 
-
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_app_bar, menu)
+        return true
+    }
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
 }
